3f9ac1f11ce665f749142c39eda6b1b4dc9b7128
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-06-29 20:03:49 +0200

    Initial commit

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,16 @@
+import PlaceEntry from "./PlaceEntry";
+
+function MainContent(): JSX.Element {
+    return (
+        <main>
+            <PlaceEntry
+            imageURL={"src/image/poveglia.jpg"}
+            placeName={"Poveglia Island"}
+            countryName={"Italy"}
+            mapURL={"https://www.google.com/maps/place/Poveglia/@45.3819737,12.328871,17z/data=!3m1!4b1!4m6!3m5!1s0x477ea550964f6259:0x6186e01392eeb1e8!8m2!3d45.3818788!4d12.331196!16zL20vMDV0bDc1?hl=en-GB&entry=ttu"}
+            text={"Poveglia Island is an abandoned island located in the Venetian Lagoon in Italy. It has a haunting history and is known as one of the most haunted places in the world."}
+            />
+        </main>
+    )
+}
+export default MainContent;
---
+++ src/components/PlaceEntry.tsx
@@ -0,0 +1,20 @@
+interface PlaceEntryProps {
+    imageURL: string;
+    placeName: string;
+    countryName: string;
+    mapURL: string;
+    text: string;
+  }
+function PlaceEntry(props:PlaceEntryProps): JSX.Element{
+    return (
+        <section>
+            <img src={props.imageURL} ></img>
+            <h2>{props.placeName}</h2>
+            <h3>{props.countryName}</h3>
+            <p>{props.text}</p>
+            <a href={props.mapURL}>Click</a>
+        </section>
+    );
+}
+
+export default PlaceEntry;
