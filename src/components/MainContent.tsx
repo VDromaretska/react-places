55a1b418f31ca9291589adb650990c7bbba6902a
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-06-29 22:50:27 +0200

    Fix picture issue

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,12 +1,14 @@
 import PlaceEntry from "./PlaceEntry";
+import povegliaImage from "./poveglia.jpg";
+
 
 function MainContent(): JSX.Element {
   return (
     <main>
       <PlaceEntry
-        imageURL={"./poveglia.jpg"}
+        imageURL={povegliaImage}
         imageAlt={"poveglia"}
-        placeName={"Poveglia Maybe Island"}
+        placeName={"Poveglia Island"}
         countryName={"Italy"}
         mapURL={
           "https://www.google.com/maps/place/Poveglia/@45.3819737,12.328871,17z/data=!3m1!4b1!4m6!3m5!1s0x477ea550964f6259:0x6186e01392eeb1e8!8m2!3d45.3818788!4d12.331196!16zL20vMDV0bDc1?hl=en-GB&entry=ttu"
