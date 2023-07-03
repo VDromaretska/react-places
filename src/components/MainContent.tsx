002a8faf404a0268209c26f4b3b5f8008b1e61af
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-07-03 15:33:46 +0200

    New branch with use of obj-in-obj as props

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,72 +1,23 @@
 import PlaceEntry from "./PlaceEntry";
-import React from "react";
 import povegliaImage from "./poveglia.jpg";
 import "./Main.css";
+import {placeArray} from "../placeArray";
+import {Place} from "../placeArray";
 
 function MainContent(): JSX.Element {
   return (
     <main className="flex-container">
-      <PlaceEntry
-        imageURL={povegliaImage}
-        imageAlt={"poveglia"}
-        placeName={"Poveglia Island"}
-        countryName={"Italy"}
-        mapURL={
-          "https://www.google.com/maps/place/Poveglia/@45.3819737,12.328871,17z/data=!3m1!4b1!4m6!3m5!1s0x477ea550964f6259:0x6186e01392eeb1e8!8m2!3d45.3818788!4d12.331196!16zL20vMDV0bDc1?hl=en-GB&entry=ttu"
-        }
-        text={
-          "Poveglia Island is an abandoned island located in the Venetian Lagoon in Italy. It has a haunting history and is known as one of the most haunted places in the world."
-        }
-      />
-      <PlaceEntry
-        imageURL={"./bran.jpg"}
-        imageAlt={"romania"}
-        placeName={"Bran Castle"}
-        countryName={"Romania"}
-        mapURL={
-          "https://www.google.com/maps/place/Bran+Castle/@45.5149022,25.364975,17z/data=!3m1!4b1!4m6!3m5!1s0x40b347e5a415de31:0xcf922792d921ab7f!8m2!3d45.5149022!4d25.3671637!16zL20vMDJnbXk3?entry=ttu"
-        }
-        text={
-          "Bran Castle, commonly known as Dracula's Castle, is a medieval fortress situated in the Transylvania region of Romania. It has a Gothic appearance and is associated with the legend of Count Dracula."
-        }
-      />
-      <PlaceEntry
-        imageURL={"./islandofdolls.jpg"}
-        imageAlt={"mexico"}
-        placeName={"Island of Dolls"}
-        countryName={"Mexico"}
-        mapURL={
-          "https://www.google.com/maps/place/Island+of+the+Dead+Dolls/@19.2903808,-99.0968924,18z/data=!3m1!4b1!4m6!3m5!1s0x85ce03d465d90e11:0xd3b68638909d159b!8m2!3d19.2901607!4d-99.0965097!16s%2Fg%2F1yprt_f_n?entry=ttu"
-        }
-        text={
-          "The Island of the Dolls, located in the Xochimilco canals near Mexico City, is adorned with thousands of creepy dolls hanging from trees and buildings. It is believed to be haunted by the spirit of a girl who drowned nearby."
-        }
-      />
-      <PlaceEntry
-        imageURL={"./forest.jpg"}
-        imageAlt={"japan"}
-        placeName={"Aokigahara Forest"}
-        countryName={"Japan"}
-        mapURL={
-          "https://www.google.com/maps/place/Aokigahara+Forest/@35.4752183,138.6379265,17z/data=!3m1!4b1!4m6!3m5!1s0x601be721e912db77:0xed043493c8ad92e1!8m2!3d35.4752183!4d138.6405014!16zL20vMDliNWMz?entry=ttu"
-        }
-        text={
-          "Aokigahara Forest, also known as the Sea of Trees or Suicide Forest, is a dense forest at the northwest base of Mount Fuji in Japan. It is infamous for its association with suicides and eerie atmosphere."
-        }
-      />
-      <PlaceEntry
-        imageURL={"./chillon.jpg"}
-        imageAlt={"Switzerland"}
-        placeName={"Château de Chillon"}
-        countryName={"Switzerland"}
-        mapURL={
-          "https://www.google.com/maps/place/Chillon+Castle/@46.4142131,6.9249175,17z/data=!3m1!4b1!4m6!3m5!1s0x478e9a5baedd65c3:0x22994259155c8307!8m2!3d46.4142131!4d6.9274924!16zL20vMDRkZHB6?entry=ttu"
-        }
-        text={
-          "Château de Chillon is a medieval castle located on the shores of Lake Geneva in Switzerland. It has a mysterious atmosphere and is known for its underground dungeons and eerie legends."
-        }
-      />
+        <PlaceEntry place = {placeArray[0]} />
+        <PlaceEntry place = {placeArray[1]} />
+        <PlaceEntry place = {placeArray[2]} />
+        <PlaceEntry place = {placeArray[3]} />
+        <PlaceEntry place = {placeArray[4]} />
+     
     </main>
   );
 }
 export default MainContent;
+
+export interface PlaceViewProps {
+    place: Place;
+}
--- src/components/PlaceEntry.tsx
+++ src/components/PlaceEntry.tsx
@@ -1,5 +1,6 @@
 import "./PlaceEntry.css";
-
+import {PlaceViewProps} from "./MainContent"
+/*
 interface PlaceEntryProps {
   imageURL: string;
   imageAlt: string;
@@ -7,15 +8,15 @@ interface PlaceEntryProps {
   countryName: string;
   mapURL: string;
   text: string;
-}
-function PlaceEntry(props: PlaceEntryProps): JSX.Element {
+} */
+function PlaceEntry(props: PlaceViewProps): JSX.Element {
   return (
     <section className="flex-card">
-      <img src={props.imageURL} alt={props.imageAlt} className="card-image" />
-      <h2>{props.placeName}</h2>
-      <h3>{props.countryName}</h3>
-      <p>{props.text}</p>
-      <a href={props.mapURL}>Click</a>
+      <img src={props.place.imageURL} alt={props.place.imageAlt} className="card-image" />
+      <h2>{props.place.placeName}</h2>
+      <h3>{props.place.countryName}</h3>
+      <p>{props.place.text}</p>
+      <a href={props.place.mapURL}>Click</a>
     </section>
   );
 }
